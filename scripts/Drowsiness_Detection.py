e76efc8f06b68521850b551bce0ac464ff7b6060
mohamedemad4 <mohamed.emad4bubble@gmail.com>
2020-02-25 14:27:00 +0200

    time instead of flag

--- scripts/Drowsiness_Detection.py
+++ scripts/Drowsiness_Detection.py
@@ -7,7 +7,6 @@ import imutils
 import threading
 import numpy as np
 from imutils import face_utils
-from flask import Flask, render_template, Response
 
 def eye_aspect_ratio(eye):
 	A = np.linalg.norm(eye[1]-eye[5])
@@ -16,18 +15,19 @@ def eye_aspect_ratio(eye):
 	ear = (A + B) / (2.0 * C)
 	return ear
 	
-thresh = 0.25
-frame_check = 20
+ear_thresh = 0.25
+seconds_with_no_eyes=2.5
+seconds_with_sub_ear=1.5
+
 detect = dlib.get_frontal_face_detector()
 predict = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")# Dat file is the crux of the code
 
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
 cap=cv2.VideoCapture(0) #"../data/glasses.mp4")
-flag=0
-eye_flag=0
+
 last_eyes=time.time()
-seconds_with_no_eyes=2.5
+last_ear_critical=time.time()
 
 while True:
 	ret, frame=cap.read()
@@ -58,15 +58,13 @@ while True:
 		cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
 		cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
 		print(ear)
-		if ear < thresh:
-			flag += 1
-			print(flag)
-			if flag >= frame_check:
+		if ear < ear_thresh:
+			if (time.time()-last_ear_critical)>seconds_with_sub_ear:
 				print("Alert")
 				cv2.putText(frame, "****************ALERT!****************", (10, 30),
 					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 				cv2.putText(frame, "****************ALERT!****************", (10,325),
 					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 		else:
-			flag = 0
+			last_ear_critical=time.time()
 cap.stop()
