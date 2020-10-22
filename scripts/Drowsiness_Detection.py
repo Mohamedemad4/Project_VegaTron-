26327f55232a3a3d06e80907d4403ce78aa493df
mohamedemad4 <mohamed.emad4bubble@gmail.com>
2020-10-22 23:27:30 +0200

    imageio writer

--- scripts/Drowsiness_Detection.py
+++ scripts/Drowsiness_Detection.py
@@ -3,6 +3,7 @@
 import cv2
 import dlib
 import time
+import imageio
 import imutils
 import threading
 import numpy as np
@@ -25,61 +26,63 @@ predict = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")# Dat fil
 
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
-cap=cv2.VideoCapture(0) #"../data/glasses.mp4")
+cap=cv2.VideoCapture('/home/daruis1/repos/cycloidSol/facerecog/out.mp4')##0) #"../data/glasses.mp4")
+writer = imageio.get_writer('cockatoo_gray.mp4', fps=15)
 
 last_eyes=time.time()
 last_ear_uncritical=time.time()
 
 while True:
-	ret, frame=cap.read()
-	if not ret:
-		print("failed to read cap")
-		continue
-	frame = imutils.resize(frame, width=450)
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	subjects = detect(gray, 0)
-	if not subjects:
-		if (time.time()-last_eyes)>seconds_with_no_eyes:
-			print("Show me those eyes")
-			blue1()
-			buzz1()
-			cv2.putText(frame, "********Show Me those Eyes********", (10, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
-			cv2.putText(frame, "****************ALERT!****************", (10,325),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0,0), 2)
-	else:  
-		blue0()
-		buzz0()
-		last_eyes=time.time()
-	for subject in subjects:
-		shape = predict(gray, subject)
-		shape = face_utils.shape_to_np(shape)#converting to NumPy Array
-		leftEye = shape[lStart:lEnd]
-		rightEye = shape[rStart:rEnd]
-		leftEAR = eye_aspect_ratio(leftEye)
-		rightEAR = eye_aspect_ratio(rightEye)
-		ear = (leftEAR + rightEAR) / 2.0
-		leftEyeHull = cv2.convexHull(leftEye)
-		rightEyeHull = cv2.convexHull(rightEye)
-		cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
-		cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
-		print(ear)
-		if ear < ear_thresh:
-			if (time.time()-last_ear_uncritical)>seconds_with_sub_ear:
-				print("Alert")
+	try:
+		ret, frame=cap.read()
+		if not ret:
+			print("failed to read cap")
+			continue
+		frame = imutils.resize(frame, width=450)
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		subjects = detect(gray, 0)
+		if not subjects:
+			if (time.time()-last_eyes)>seconds_with_no_eyes:
+				print("Show me those eyes")
+				blue1()
 				buzz1()
-				red1()
-				cv2.putText(frame, "****************ALERT!****************", (10, 30),
-					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-				cv2.putText(frame, "****************ALERT!****************", (10,325),
-					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-		else:
+				cv2.putText(frame, "********Show Me those Eyes********", (10, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
+				cv2.putText(frame, "****************ALERT!****************", (10,325),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0,0), 2)
+		else:  
+			blue0()
 			buzz0()
-			red0()
-			last_ear_uncritical=time.time()
-	if dbg:
-		cv2.imshow("Frame", frame)
-		key = cv2.waitKey(1) & 0xFF
-		if key == ord("q"):
-			break
-if dbg:
-    cv2.destroyAllWindows()
-cap.stop()
+			last_eyes=time.time()
+		for subject in subjects:
+			shape = predict(gray, subject)
+			shape = face_utils.shape_to_np(shape)#converting to NumPy Array
+			leftEye = shape[lStart:lEnd]
+			rightEye = shape[rStart:rEnd]
+			leftEAR = eye_aspect_ratio(leftEye)
+			rightEAR = eye_aspect_ratio(rightEye)
+			ear = (leftEAR + rightEAR) / 2.0
+			leftEyeHull = cv2.convexHull(leftEye)
+			rightEyeHull = cv2.convexHull(rightEye)
+			cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+			cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+			print(ear)
+			if ear < ear_thresh:
+				if (time.time()-last_ear_uncritical)>seconds_with_sub_ear:
+					print("Alert")
+					buzz1()
+					red1()
+					cv2.putText(frame, "****************ALERT!****************", (10, 30),
+						cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+					cv2.putText(frame, "****************ALERT!****************", (10,325),
+						cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+			else:
+				buzz0()
+				red0()
+				last_ear_uncritical=time.time()
+		if dbg:
+			writer.append_data(cv2.cvtColor(frame,cv2.COLOR_BGR2RGB))
+			
+	except KeyboardInterrupt:
+		writer.close()
+		print("REL")
+		cap.release()
+		exit()
